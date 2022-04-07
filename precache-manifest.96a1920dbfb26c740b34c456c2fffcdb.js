2006041431c54f1cd3e8af3304a9f92f729fb78b
OG Puzzle <ogpuzzle@tutanota.com>
2022-04-07 03:22:17 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.96a1920dbfb26c740b34c456c2fffcdb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5503b204cae2450a27dd6684b33480e0",
+    "url": "/martian_madness/index.html"
+  },
+  {
+    "revision": "2da7ede72c3e1a0509fd",
+    "url": "/martian_madness/static/css/main.ff7c3ae5.chunk.css"
+  },
+  {
+    "revision": "d5105f66ae6738d11342",
+    "url": "/martian_madness/static/js/2.58589d47.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/martian_madness/static/js/2.58589d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da7ede72c3e1a0509fd",
+    "url": "/martian_madness/static/js/main.d5c0fa05.chunk.js"
+  },
+  {
+    "revision": "bbae3bbb2ac62fffb6cc",
+    "url": "/martian_madness/static/js/runtime-main.71becaf2.js"
+  },
+  {
+    "revision": "72da254f6fc013dea7017cd7516e00ed",
+    "url": "/martian_madness/static/media/clear-giga.72da254f.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/martian_madness/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/martian_madness/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/martian_madness/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/martian_madness/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "b69c0b68f886a47813f05b3763e5e1a1",
+    "url": "/martian_madness/static/media/features-split-image-04.b69c0b68.png"
+  },
+  {
+    "revision": "371482a3dadf269c25e3e0da328e69b1",
+    "url": "/martian_madness/static/media/features-split-image-05.371482a3.png"
+  },
+  {
+    "revision": "d3af5ba34ba3a7198986fbb385ad53b6",
+    "url": "/martian_madness/static/media/features-split-image-06.d3af5ba3.png"
+  },
+  {
+    "revision": "e903012a5e1c553f96d701254c4cee29",
+    "url": "/martian_madness/static/media/features-split-image-07.e903012a.png"
+  },
+  {
+    "revision": "58dfbafee01d3711ed2f680c01bc9183",
+    "url": "/martian_madness/static/media/illustration-section-01.58dfbafe.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/martian_madness/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "8d33c4486eb1ad415990b65dfde3b4bc",
+    "url": "/martian_madness/static/media/img-placeholder.8d33c448.png"
+  },
+  {
+    "revision": "feb82b7e0a88d8b03bb0ae4eebbea409",
+    "url": "/martian_madness/static/media/logo.feb82b7e.svg"
+  }
+]);
