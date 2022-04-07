4ba41ed6c4e2738a3ad9b0535039b8692fd0295c
OG Puzzle <ogpuzzle@tutanota.com>
2022-04-07 04:44:34 -0700

    Updates

---
+++ precache-manifest.65e140a3dd710fbc76fac5394cd7cc93.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b684118fc9665a7d19dbf4504edebfeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4550508b9a3b04a33bae",
+    "url": "/static/css/main.dbbd59b4.chunk.css"
+  },
+  {
+    "revision": "d5105f66ae6738d11342",
+    "url": "/static/js/2.58589d47.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.58589d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4550508b9a3b04a33bae",
+    "url": "/static/js/main.9b4491a2.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "72da254f6fc013dea7017cd7516e00ed",
+    "url": "/static/media/clear-giga.72da254f.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "b69c0b68f886a47813f05b3763e5e1a1",
+    "url": "/static/media/features-split-image-04.b69c0b68.png"
+  },
+  {
+    "revision": "371482a3dadf269c25e3e0da328e69b1",
+    "url": "/static/media/features-split-image-05.371482a3.png"
+  },
+  {
+    "revision": "d3af5ba34ba3a7198986fbb385ad53b6",
+    "url": "/static/media/features-split-image-06.d3af5ba3.png"
+  },
+  {
+    "revision": "e903012a5e1c553f96d701254c4cee29",
+    "url": "/static/media/features-split-image-07.e903012a.png"
+  },
+  {
+    "revision": "58dfbafee01d3711ed2f680c01bc9183",
+    "url": "/static/media/illustration-section-01.58dfbafe.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "8d33c4486eb1ad415990b65dfde3b4bc",
+    "url": "/static/media/img-placeholder.8d33c448.png"
+  },
+  {
+    "revision": "feb82b7e0a88d8b03bb0ae4eebbea409",
+    "url": "/static/media/logo.feb82b7e.svg"
+  }
+]);
