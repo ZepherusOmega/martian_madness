e64cab366698094632fa11d726783f80aed63b97
OG Puzzle <ogpuzzle@tutanota.com>
2022-04-07 06:48:27 -0700

    Updates

--- precache-manifest.a99c74bdc431b4ac4a7a87c9123ff147.js
+++ precache-manifest.a99c74bdc431b4ac4a7a87c9123ff147.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f30c9f278359f37ffd1ac9167b57680e",
+    "revision": "186d67b84f9204243ecc3ea4a41aa391",
     "url": "/index.html"
   },
   {
-    "revision": "a20afe2102b32989a947",
+    "revision": "abdcd7202f9142612ca3",
     "url": "/static/css/main.dbbd59b4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.58589d47.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a20afe2102b32989a947",
-    "url": "/static/js/main.5f4e6dda.chunk.js"
+    "revision": "abdcd7202f9142612ca3",
+    "url": "/static/js/main.71866a3b.chunk.js"
   },
   {
     "revision": "c307e69827cf23a9bfac",
@@ -68,8 +68,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/illustration-section-02.63d0555d.svg"
   },
   {
-    "revision": "8d33c4486eb1ad415990b65dfde3b4bc",
-    "url": "/static/media/img-placeholder.8d33c448.png"
+    "revision": "3e14502b6abf723a259ecafb7c6b3c4c",
+    "url": "/static/media/img-placeholder.3e14502b.png"
   },
   {
     "revision": "feb82b7e0a88d8b03bb0ae4eebbea409",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ead5d73524ae2034f6ec3b3d0f9d7abd.js"
+  "/precache-manifest.a99c74bdc431b4ac4a7a87c9123ff147.js"
 );
 
 self.addEventListener('message', (event) => {
